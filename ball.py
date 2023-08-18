e0f1e0bb30f46bb866850f8ad83fab7245149d9e
nitesh-19 <niteshgohil25@gmail.com>
2023-08-18 22:32:15 +0530

    Added ball rebound in the width dimension

--- ball.py
+++ ball.py
@@ -1,6 +1,9 @@
 from turtle import Turtle
+from random import randrange
 
 BALL_COLOR = "white"
+SCREEN_WIDTH = 1900
+SCREEN_HEIGHT = 1000
 
 
 class Ball(Turtle):
@@ -9,3 +12,15 @@ class Ball(Turtle):
         self.penup()
         self.color(BALL_COLOR)
         self.shape("circle")
+        hypotenuse = ((SCREEN_WIDTH / 2) ** 2 + (SCREEN_HEIGHT / 2) ** 2) ** (1 / 2)
+        angle_allowed = int(SCREEN_HEIGHT / 2 / (hypotenuse / 90))
+        self.direction(randrange(180 - angle_allowed, 180 + angle_allowed, 1))
+
+    def move(self):
+        self.forward(30)
+
+    def direction(self, angle):
+        self.setheading(to_angle=angle)
+
+    def rebound(self):
+        self.setheading((90 - self.heading()) * 2 + self.heading())
