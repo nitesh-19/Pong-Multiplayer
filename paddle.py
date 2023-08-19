e7f95b4e365b13a609825126e4ab6a8b17fa8715
nitesh-19 <niteshgohil25@gmail.com>
2023-08-19 13:17:46 +0530

    Made paddle width definable for each paddle

--- paddle.py
+++ paddle.py
@@ -1,19 +1,19 @@
 from turtle import Turtle
 
-PADDLE_COLOR = "white"
-PADDLE_WIDTH = 20
+PADDLE_COLOR = "gray"
+paddle_width = 2
 PADDLE_STEP = 30
 
 
 class Paddle(Turtle):
-    def __init__(self):
+    def __init__(self,paddle_width):
         super().__init__()
         self.penup()
         self.shape("square")
         self.color(PADDLE_COLOR)
-        self.shapesize(stretch_len=PADDLE_WIDTH, stretch_wid=1)
+        self.shapesize(stretch_len=paddle_width, stretch_wid=1)
         self.setheading(90)
-        self.paddle_bound = PADDLE_WIDTH * 12.5
+        self.paddle_bound = paddle_width * 12.5
 
     def move_up(self):
         self.forward(PADDLE_STEP)
