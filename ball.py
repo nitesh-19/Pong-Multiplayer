7d9f7996c1300f7a725e13a362a5a0f3fc26ccc7
nitesh-19 <niteshgohil25@gmail.com>
2023-08-20 12:43:19 +0530

    Rewritten randomized ball serve bounds.

--- ball.py
+++ ball.py
@@ -1,5 +1,5 @@
 from turtle import Turtle
-from random import randrange
+from random import randrange, randint
 from Settings import *
 
 
@@ -9,8 +9,14 @@ class Ball(Turtle):
         self.penup()
         self.color(BALL_COLOR)
         self.shape("circle")
-        hypotenuse = ((SCREEN_WIDTH / 2) ** 2 + (SCREEN_HEIGHT / 2) ** 2) ** (1 / 2)
-        self.angle_allowed = int(SCREEN_HEIGHT / 2 / (hypotenuse / 90)) - 20
+        hypotenuse = ((((SCREEN_WIDTH / 2) ** 2) + ((SCREEN_HEIGHT / 2) ** 2)) ** 0.5)
+        self.restricted_area = 10  # restricted angle from x axis
+        self.angle_allowed = int(SCREEN_HEIGHT / 2 / (hypotenuse / 90)) - self.restricted_area - 12
+        self.sectors = [(180 - self.restricted_area, 180 - self.restricted_area - self.angle_allowed),
+                        (180 + self.restricted_area + self.angle_allowed, 180 + self.restricted_area),
+                        (self.restricted_area, self.angle_allowed),
+                        (360 - self.restricted_area - self.angle_allowed, 360 - self.restricted_area)]
+
         self.direction("l")
 
     def move(self):
@@ -18,9 +24,11 @@ class Ball(Turtle):
 
     def direction(self, start_side):
         if start_side == "l":
-            self.setheading(randrange(160 - self.angle_allowed, 200 + self.angle_allowed, 1))
+            random_sector = randint(0, 1)
+            self.setheading(randrange(self.sectors[random_sector][1], self.sectors[random_sector][0], 1))
         elif start_side == "r":
-            self.setheading(randrange(20 - self.angle_allowed, 340 + self.angle_allowed, 1))
+            random_sector = randint(2, 3)
+            self.setheading(randrange(self.sectors[random_sector][0], self.sectors[random_sector][1], 1))
 
     def go_home(self):
         self.goto(0, 0)
