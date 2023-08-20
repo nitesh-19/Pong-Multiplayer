26132c5415f65a4c748e835d9a51116ce77d5f35
nitesh-19 <niteshgohil25@gmail.com>
2023-08-20 10:08:25 +0530

    Added Settings.py to contain all configurable settings.

--- pong_main.py
+++ pong_main.py
@@ -3,14 +3,7 @@ from paddle import Paddle
 from ball import Ball
 from main_screen import MainScreen
 from scoreboard import Scoreboard
-
-SCREEN_WIDTH = 720
-SCREEN_HEIGHT = 480
-
-SCORE_TO_WIN = 3
-
-# GAME_MODE - Options {1, 2} - Change to 1 for Single player or 2 for Multiplayer
-GAME_MODE = 2
+from Settings import *
 
 screen = MainScreen(SCREEN_WIDTH=SCREEN_WIDTH, SCREEN_HEIGHT=SCREEN_HEIGHT)
 paddle1 = Paddle()
@@ -33,7 +26,6 @@ ball = Ball()
 
 while game_on:
     ball.move()
-    screen.screen.update()
     screen.screen.update()
     time.sleep(0.0000001)
     if ball.xcor() <= 0:  # If ball is in the left half of the screen, then only process the relevant code to that half
