0f21b7e7a217a1d0308d15c8bd45ef8e18b62618
nitesh-19 <niteshgohil25@gmail.com>
2023-08-20 10:54:38 +0530

    Tweaked the angle allowed and added forward movement after rebound to eliminate hysteresis

--- ball.py
+++ ball.py
@@ -10,8 +10,7 @@ class Ball(Turtle):
         self.color(BALL_COLOR)
         self.shape("circle")
         hypotenuse = ((SCREEN_WIDTH / 2) ** 2 + (SCREEN_HEIGHT / 2) ** 2) ** (1 / 2)
-        self.angle_allowed = int(SCREEN_HEIGHT / 2 / (hypotenuse / 90)) - 10
-        print(self.angle_allowed)
+        self.angle_allowed = int(SCREEN_HEIGHT / 2 / (hypotenuse / 90)) - 20
         self.direction("l")
 
     def move(self):
@@ -19,15 +18,17 @@ class Ball(Turtle):
 
     def direction(self, start_side):
         if start_side == "l":
-            self.setheading(randrange(160 - self.angle_allowed, 160 + self.angle_allowed, 1))
+            self.setheading(randrange(160 - self.angle_allowed, 200 + self.angle_allowed, 1))
         elif start_side == "r":
-            self.setheading(randrange(20 - self.angle_allowed, 20 + self.angle_allowed, 1))
+            self.setheading(randrange(20 - self.angle_allowed, 340 + self.angle_allowed, 1))
 
     def go_home(self):
         self.goto(0, 0)
 
     def rebound_in_width(self):
         self.setheading((180 - self.heading()))
+        self.forward(11)
 
     def rebound_in_height(self):
         self.setheading(360 - self.heading())
+        self.forward(11)
